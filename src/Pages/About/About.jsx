2ceebebf5367887c676ac5deae1d9941c8fd5aa6
Anna Velarde <annavelardevilaseca@gmail.com>
2023-07-22 18:17:42 +0100

    done contact page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,19 +3,7 @@ import "./About.css";
 function About() {
   return (
     <div>
-      <div className="wrapper">
-        <figure className="card">
-          <figcaption>
-            <blockquote>
-              We shape our tools and then the tools shape us.
-            </blockquote>
-            <cite>
-              Attributed to Winston Churchill, Marshall McLuhan, and John
-              Culkin.
-            </cite>
-          </figcaption>
-        </figure>
-      </div>
+      <h1>hi</h1>
     </div>
   );
 }
