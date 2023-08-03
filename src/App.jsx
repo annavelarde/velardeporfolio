0a9f25b01ef06e28a1014a8e486651068d1744fc
Anna Velarde <annavelardevilaseca@gmail.com>
2023-08-03 19:45:21 +0100

    again

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Navbar from "./Components/Navbar/navbar.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
 
 function App() {
   return (
