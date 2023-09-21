9e787f752815902a33828f04242d907b568f1d03
Anna Velarde <annavelardevilaseca@gmail.com>
2023-09-21 19:23:40 +0100

    adding images, links and text in the projects card

--- src/dataprojects.js
+++ src/dataprojects.js
@@ -1,21 +1,25 @@
-import calathea from "/calathea.jpg";
+import calathea from "/calatheaapp.png";
 import cluegame from "/cluegame.png";
+import cargame from "/cargame.png";
+import plantapp from "/plantapp.png";
 
 const projects = [
   {
     idProyecto: "1",
-    title: "Clue Game 🔎",
+    title: "Clue Game🔎",
     description:
       "Using only Vanilla Javascript and CSS to manipulate the Document Object Model (DOM), we've created an exciting new game. It's a game of chance that involves selecting a murderer, weapon, and location in order to solve a murder mystery. This project is sure to provide hours of entertainment for players of all ages. Deployed on the powerful Netlify platform and it has a responsive design.",
     leng: ["Javascript (ES6)", "CSS", "HTML5"],
     ownedBy: "Anna Velarde",
     year: "2023",
     url: cluegame,
+    link: "https://thecluegame.netlify.app/",
   },
   {
     idProyecto: "2",
-    title: "PlantApp",
-    description: "FullStack App",
+    title: "Plant weblog - MERN WebApp 🌿",
+    description:
+      "This application utilizes the cutting-edge technologies of MongoDB, Express, ReactJS and NodeJS (MERN), giving you unparalleled performance and flexibility. With comprehensive CRUD operations available, you'll have full control over your data. Plus, our MVC and ReactJS data management tools make it easy to keep your application running smoothly.",
     leng: [
       "Javascript",
       "CSS",
@@ -27,25 +31,28 @@ const projects = [
     ],
     ownedBy: "Anna Velarde",
     year: "2022",
-    url: "https://img.freepik.com/fotos-gratis/close-up-de-uma-flor-roxa_181624-25863.jpg?t=st=1691680311~exp=1691680911~hmac=b955dc6be4ddb50fb5f6dee2866db2dc8b76f19f38b100a386bf8b73f3409c1e",
+    url: plantapp,
+    link: "https://plantsapp.netlify.app/",
   },
   {
     idProyecto: "3",
-    title: "CarGame",
-    description: "",
+    title: "SpeedCar Game🏎",
+    description:
+      "SpeedCar is a game built using Vanilla Javascript and P5JS. The objective of the game is to collect points by obtaining stars. However, you must be careful and avoid the obstacles on the road, as they can cause you to lose the game.",
     leng: ["Javascript", "HTML", "CSS"],
     ownedBy: "Anna Velarde",
     year: "2022",
-    url: "https://img.freepik.com/fotos-gratis/close-up-de-uma-flor-roxa_181624-25863.jpg?t=st=1691680311~exp=1691680911~hmac=b955dc6be4ddb50fb5f6dee2866db2dc8b76f19f38b100a386bf8b73f3409c1e",
+    url: cargame,
+    link: "https://annavelarde.github.io/game/",
   },
   {
     idProyecto: "4",
     title: "Calathea App",
     description:
-      "Looking for a powerful back-end web application that's perfect for plant bloggers? - Project: Calathea - MEN WebApp. This app was built using Javascript, Express, NodeJS, and MongoDB. Offers robust CRUD operations for both blogs and users, all while utilizing the highly efficient MVC architecture. A sleek UI design that incorporates the Bootstrap framework and handlebars as a simple templating engine (front-end). The project is deployed on Render, ensuring maximum accessibility for users all around the world",
+      "Looking for a powerful back-end web application that's perfect for plant bloggers? - Project: Calathea - MEN WebApp. This app was built using Javascript, Express, NodeJS, and MongoDB. Offers robust CRUD operations for both blogs and users, all while utilizing the highly efficient MVC architecture. A sleek UI design that incorporates the Bootstrap framework and handlebars as a simple templating engine (front-end).",
     // Technologies: Javascript(ES6) · Express · NodeJS · MongoDB · Bootstrap · Cloudinary",
     leng: ["Javascript", "CSS", "HTML", "Handlebars", "MongoDB", "Cloudinary"],
-    ownedBy: ["Juan Carlos Eugenio", "Anna Velarde"],
+    ownedBy: ["Juan Carlos Eugenio", " & ", "Anna Velarde"],
     year: "2022",
     url: calathea,
     link: "https://calathea-blog.onrender.com/",
