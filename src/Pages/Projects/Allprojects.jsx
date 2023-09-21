ef20fbb334769d9706267e9c85520d6b29bded1e
Anna Velarde <annavelardevilaseca@gmail.com>
2023-09-21 13:05:15 +0100

    updating project page

--- src/Pages/Projects/Allprojects.jsx
+++ src/Pages/Projects/Allprojects.jsx
@@ -10,13 +10,6 @@ function Allprojects() {
       <div className="containerProjects">
         <h1 className="titleProjects">Check out my projects </h1>
         <p className="mainText">
-          {/* As a dedicated web developer, I meticulously define objectives and
-        target audience, researching trends and exploring inspiration. Using
-        valuable data and paying close attention to detail, I immerse myself in
-        developing wireframes and prototypes, ensuring a unique user experience
-        design. Once the design stage is complete, my focus shifts to writing
-        clean and optimized code, guaranteeing a responsive and high-performance
-        website. */}
           As a dedicated <b>web developer</b> specializing in creating MERN
           (MongoDB, Express.js, React, Node.js) applications, my passion lies in
           developing immersive and user-centric digital experiences. With a keen
@@ -28,40 +21,40 @@ function Allprojects() {
           <br />
           <br />
           Please find below the projects I have been working on 🖥️
-          {/* <i>
-          Let&apos;s collaborate to create web applications that not only meet
-          your requirements but also exceed your expectations.
-        </i> */}
         </p>
       </div>
-
       <section className="cards">
         <div className="projects">
           {projects.map((project) => (
-            <article className="card card--1" key={project.idProyecto}>
-              {/* <div className="card__img"></div>
-              <a href="#" className="card_link">
-                <div className="card__img--hover"></div>
-              </a> */}
+            <article className="card" key={project.idProyecto}>
               <div className="card__img">
                 <img src={project.link} alt={project.title} />
-              </div>
-              <a href="#" className="card_link">
-                <div className="card__img--hover">
-                  <img src={project.url} alt={project.title} />
-                </div>
-              </a>
-              <div className="card__info">
-                <p className="card__category"> {project.title}</p>
-                <h3 className="card__description">{project.description}</h3>
-                <span className="card__by">
-                  <p className="card__author" title="author">
-                    by {project.ownedBy}
-                  </p>
-                  {/* <Link to="#" className="view-projects-button button button4">
+              </div>{" "}
+              <div className="containerCard">
+                <a href="#" className="card_link">
+                  <div className="card__img--hover"> </div>
+                </a>
+                <a href="#" className="card_link">
+                  <div className="card__img--hover">
+                    <img
+                      className="sizeImage"
+                      src={project.url}
+                      alt={project.title}
+                    />
+                  </div>
+                </a>
+                <div className="card__info">
+                  <p className="card__category"> {project.title}</p>
+                  <h3 className="card__description">{project.description}</h3>
+                  <span className="card__by">
+                    <p className="card__author" title="author">
+                      by {project.ownedBy}
+                    </p>
+                    {/* <Link to="#" className="view-projects-button button button4">
                     View Project
                   </Link> */}
-                </span>
+                  </span>
+                </div>
               </div>
             </article>
           ))}
