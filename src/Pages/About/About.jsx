b14fc870c98022c994cb38313878e63fbe4c1b6f
Anna Velarde <annavelardevilaseca@gmail.com>
2023-11-12 17:35:03 +0000

    pushing new old updates to github

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,18 +1,18 @@
-// import "./About.css";
+import "./About.css";
 import { Chrono } from "react-chrono";
 
 function About() {
   const items = [
     {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
+      title: "Jan 2022 - Now",
+      cardTitle: "3D/Lighting Specialist,",
       cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to a destroyer during the evacuation from Dunkirk.",
+        "Working on projects using Revit (BIM 360) and ReluxCAD.Designing 3D spaces/Metaverse/VR.",
       cardDetailedText: ["paragraph1", "paragraph2"],
-      timelineContent: <div>Custom content</div>,
+      timelineContent: <div>Hoare Lea. Bristol, UK</div>,
     },
     {
-      title: "Timeline title 1",
+      title: "Jan 2020 - Jan 2022",
       cardTitle: "Card Title 1",
       items: [
         { cardTitle: "nested card title 1" },
@@ -20,16 +20,6 @@ function About() {
       ],
     },
     { title: "Timeline title 2", cardTitle: "Card Title 2" },
-
-    {
-      title: "May 1940",
-      cardTitle: "Dunkirk",
-      url: "http://www.history.com",
-      cardSubtitle:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-      cardDetailedText:
-        "Men of the British Expeditionary Force (BEF) wade out to..",
-    },
     {
       title: "May 1940",
       cardTitle: "Dunkirk",
@@ -42,46 +32,74 @@ function About() {
   ];
 
   return (
-    <div style={{ width: "600px", height: "1950px" }}>
-      <Chrono
-        items={items}
-        mode="VERTICAL_ALTERNATING"
-        mediaSettings={{ align: "center", fit: "contain" }}
-        enableBreakPoint
-        theme={{
-          primary: "teal",
-          secondary: "white",
-          cardBgColor: "transparent",
-          cardBgColorActive: "black",
-          titleColor: "black",
-          titleColorActive: "lightpink",
-          cardDetailedTextColor: "black",
-          cardDetailedTextColorActive: "teal",
-          cardTitleColor: "teal",
-          cardTitleColorActive: "lightpink",
-        }}
-        fontSizes={{
-          cardSubtitle: "0.85rem",
-          cardText: "0.8rem",
-          cardTitle: "1.2rem",
-          title: "1.1rem",
-        }}
-        buttonTexts={{
-          first: "Jump to First",
-          last: "Jump to Last",
-          next: "Next",
-          previous: "Previous",
-        }}
-        classNames={{
-          card: "my-card",
-          cardMedia: "my-card-media",
-          cardSubTitle: "my-card-subtitle",
-          cardText: "my-card-text",
-          cardTitle: "my-card-title",
-          controls: "my-controls",
-          title: "my-title",
-        }}
-      />
+    <div className="aboutContainer">
+      <div className="sectionsContainer">
+        <div>
+          <img
+            src="https://media.licdn.com/dms/image/D4E03AQFqsiryuS_u3A/profile-displayphoto-shrink_800_800/0/1680116012895?e=2147483647&v=beta&t=5b9tetwm3YOVOuO50o-FvIzE-MjIFztYfa2YPwYsl0g"
+            alt=""
+            style={{ width: "100px", height: "100px" }}
+          />
+          <h1>About.</h1>
+          <h3>
+            MERN Full Stack Developer - Javascript (ES6), MongoDB, Express,
+            React, NodeJS, HTML & CSS
+          </h3>
+          <p>
+            As a full-stack web developer, I am passionate about creating
+            engaging and responsive websites that enhance user experiences. I
+            thrive on the challenge of problem-solving and love working with
+            code to find solutions that exceed client expectations. My strong
+            technical skills and ability to quickly adapt to new technologies
+            enable me to approach projects efficiently and effectively.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h2>Experience.</h2>{" "}
+        <div style={{ width: "500px", height: "1950px" }}>
+          <Chrono
+            items={items}
+            mode="VERTICAL_ALTERNATING"
+            mediaSettings={{ align: "center", fit: "contain" }}
+            enableBreakPoint
+            theme={{
+              primary: "teal",
+              secondary: "transparent",
+              cardBgColor: "transparent",
+              cardBgColorActive: "black",
+              titleColor: "black",
+              titleColorActive: "lightpink",
+              cardDetailedTextColor: "black",
+              cardDetailedTextColorActive: "teal",
+              cardTitleColor: "teal",
+              cardTitle: "teal",
+              cardTitleColorActive: "lightpink",
+            }}
+            fontSizes={{
+              cardSubtitle: "0.85rem",
+              cardText: "0.8rem",
+              cardTitle: "1.2rem",
+              title: "1.1rem",
+            }}
+            buttonTexts={{
+              first: "Jump to First",
+              last: "Jump to Last",
+              next: "Next",
+              previous: "Previous",
+            }}
+            classNames={{
+              card: "my-card",
+              cardMedia: "my-card-media",
+              cardSubTitle: "my-card-subtitle",
+              cardText: "my-card-text",
+              cardTitle: "my-card-title",
+              controls: "my-controls",
+              title: "my-title",
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
